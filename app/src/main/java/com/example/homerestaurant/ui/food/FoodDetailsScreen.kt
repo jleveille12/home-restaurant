4da2a074dfd77190256262662478b3180a3be046
John Leveille <jleveille1212@gmail.com>
2025-09-10 21:28:03 -0500

    Added Delete capability to FoodDetailsScreen.kt

--- app/src/main/java/com/example/homerestaurant/ui/food/FoodDetailsScreen.kt
+++ app/src/main/java/com/example/homerestaurant/ui/food/FoodDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.homerestaurant.ui.food
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,18 +16,26 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.homerestaurant.HomeRestaurantTopAppBar
 import com.example.homerestaurant.R
@@ -103,6 +112,7 @@ private fun FoodDetailsBody(
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
     Column(
         modifier = modifier
     ) {
@@ -113,11 +123,47 @@ private fun FoodDetailsBody(
             Spacer(
                 modifier = Modifier.weight(1f)
             )
-            Icon(
-                imageVector = Icons.Default.Delete,
-                contentDescription = stringResource(R.string.delete)
-            )
+            IconButton( onClick = { deleteConfirmationRequired = true } )
+            {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = stringResource(R.string.delete),
+                )
+            }
         }
-
     }
+    if (deleteConfirmationRequired) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                deleteConfirmationRequired = false
+                onDelete()
+            },
+            onDeleteCancel = { deleteConfirmationRequired = false },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text(stringResource(R.string.delete_confirmation_title)) },
+        text = { Text(stringResource(R.string.delete_confirmation_text)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = stringResource(R.string.delete_confirmation_cancel))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = stringResource(R.string.delete_confirmation_confirm))
+            }
+        }
+    )
 }
